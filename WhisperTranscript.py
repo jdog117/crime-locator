4ef27a5e39b1b07644474112ab5ba57e08c069d3
jdog117 <jspcasey@gmail.com>
2024-03-02 08:25:55 -0800

    bert location extraction
    Super slow right now. Will try some other whisper models potentioally and might use a gpt for location extraction because some inferencing might need to be done to tell where a person is.

--- WhisperTranscript.py
+++ WhisperTranscript.py
@@ -1,5 +1,30 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 import whisper
 
+# using medium model, 1.42GB
 model = whisper.load_model("medium")
 result = model.transcribe("1600_radio_audio.wav")
-print(result["text"])
+transcription = result["text"]
+print(transcription)
+
+'''
+    BERT model for Named Entity Recognition (NER)
+    AKA this model extracts named entities like locations "LOC" 
+'''
+
+# Load pre-trained BERT model for NER
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+#example_text = "I live near Broadway and 5th Avenue in New York City."
+ner_results = nlp(transcription)
+
+# Print the extracted entities if it is a location
+for entity in ner_results:
+    if entity["entity"] in ["B-LOC", "I-LOC"]:
+        print(f"Location: {entity['word']}")
+
+'''
+try bert question answer model
+'''

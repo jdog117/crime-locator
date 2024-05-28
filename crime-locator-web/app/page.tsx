c58a468498952b435e90b71b6097b5027f9f3cba
jdog117 <jspcasey@gmail.com>
2024-05-28 10:20:10 -0700

    ui update

--- crime-locator-web/app/page.tsx
+++ crime-locator-web/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { connectToBluetooth } from "@/services/bluetooth";
 import { playAudio } from "@/services/audio";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
     const [isReceivingAudio, setIsReceivingAudio] = useState(false);
@@ -41,19 +42,19 @@ export default function Home() {
     };
 
     return (
-        <div>
-            <h1 className="m-20 text-xl">Crime Locator Web App</h1>
-            <p className="m-20 text-xl">
+        <div className="m-20">
+            <h1 className="my-20 text-2xl font-bold">Crime Locator Web App</h1>
+            <p className="mt-20 text-xl">
                 {isReceivingAudio
                     ? "Receiving audio!"
                     : "Connecting to ESP32 via Bluetooth..."}
             </p>
             {error && (
-                <p className="m-20 text-lg text-red-600">Error: {error}</p>
+                <p className="my-5 text-lg text-red-600">Error: {error}</p>
             )}
-            <button onClick={handleReconnectClick} className="m-20">
+            <Button onClick={handleReconnectClick} className="my-5">
                 Reconnect
-            </button>
+            </Button>
         </div>
     );
 }
